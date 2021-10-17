370f18ce2ed060606495bd75df1e477d5c8fb483
valentabadalyants <vvbadalyants@edu.hse.ru>
2021-10-17 09:59:12 +0300

    some message

---
+++ 01 module/01 seminar/work/seminar/alltasks.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Task16
+{
+    struct Point
+    {
+        public double x, y;
+    }
+
+
+
+    class Program
+    {
+        static bool Inhere(Point point)
+        {
+            // Думаем, что центр круга в начале коодинат, т.к. не указано в условии.
+            if (Math.Sqrt(point.x * point.x + point.y * point.y) <= 2)
+            {
+                double tg = point.y / point.x;
+
+                return tg < 1 && point.x >= 0;
+            }
+
+            return false;
+        }
+
+        static void Main(string[] args)
+        {
+            Point point = new Point { x = 0.5, y = -0.5 }; // Initialize here.
+
+            Console.WriteLine(Inhere(point));
+
+        }
+    }
+}
